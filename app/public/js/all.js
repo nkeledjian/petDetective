b1330bed782171ff3a51a26fab5799a66fe37bfa
Laza <postlaza@gmail.com>
2019-01-21 20:35:52 -0800

    styling

--- app/public/js/all.js
+++ app/public/js/all.js
@@ -7,27 +7,28 @@ $.get("/api", function (data) {
   // for each pet that our server sends us back
   for (var i = 0; i < data.length; i++) {
     // create a parent div for the oncoming elements
-    var wellSection = $("<div class='card'>");
+    var wellSection = $("<div class='card bg-warning'>");
     // add a class to this div: 'well'
     wellSection.addClass("well");
     // add an id to the well to mark which well it is
     wellSection.attr("id", "pet-well-" + i);
     // append the well to the well section
     $("#well-section").append(wellSection);
+    $("#pet-well-" + i).append("<h1 class='bg-danger text-center'>" +data[i].petStatus+ " " + data[i].petType + "</h1>");
+    $("#pet-well-" + i).append("<h1>" + data[i].name + "<h1></p>");
     // Now add all of our pet data to the well we just placed on the page
-    $("#pet-well-" + i).append("<div><img src=" + data[i].petImgUrl + " style='width: 250px;'></div>");
+    $("#pet-well-" + i).append("<div ><img src=" + data[i].petImgUrl + " style='width: 400px;'></div>");
+    
     // make the name an h2,
-    $("#pet-well-" + i).append("<h2>Name: " + data[i].name + "</h2>");
-    $("#pet-well-" + i).append("<h3>Pet Status: " + data[i].petStatus + "</h3>");
-    // the role an h3,
-    $("#pet-well-" + i).append("<h3>Number: " + data[i].number + "</h3>");
+  
+    
+    
     $("#pet-well-" + i).append("<h3>Address: " + data[i].address + "</h3>");
-    // the age an h3,
-    $("#pet-well-" + i).append("<h3>Pet Type: " + data[i].petType + "</h3>");
-    // the pet color an h3.
-    $("#pet-well-" + i).append("<h3>Pet Color: " + data[i].petColor + "</h3>");
+
+    $("#pet-well-" + i).append("<h3>petColor: " + data[i].petColor + "</h3>");
     // and the pet size an h3.
-    $("#pet-well-" + i).append("<h3>Pet Size: " + data[i].petSize + "</h3>");
+    $("#pet-well-" + i).append("<h3>petSize: " + data[i].petSize + "</h3>");
+    $("#pet-well-" + i).append("<h3 class='float-left'>Number: " + data[i].number + "</>");
     
   }
 });
